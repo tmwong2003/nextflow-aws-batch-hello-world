376092d38101b3a61408434ca5912022835ddb22
Theodore Wong <tmw@tmwong.org>
2025-10-31 13:02:00 -0700

    Switched to terraform-aws-batch module for Batch configuration

--- main.tf
+++ main.tf
@@ -35,111 +35,90 @@ provider "aws" {
 # Batch compute environment and job queue configuration
 #
 
-resource "aws_batch_compute_environment" "nf_ec2_ce" {
-  name_prefix = "${var.service_name}-"
+module "nf_batch" {
+  source = "terraform-aws-modules/batch/aws"
 
-  compute_resources {
-    allocation_strategy = "BEST_FIT_PROGRESSIVE"
+  # Based on the example from
+  # https://github.com/terraform-aws-modules/terraform-aws-batch/blob/57ded354043005e2b0f8e67d85eaa39bedabd682/examples/ec2/main.tf
 
-    instance_role = aws_iam_instance_profile.nf_ecs_instance_profile.arn
+  # The module attaches AmazonEC2ContainerServiceforEC2Role automatically
+  instance_iam_role_name        = "${var.service_name}EcsInstanceRole"
+  instance_iam_role_path        = "/batch/"
+  instance_iam_role_description = "IAM instance role/profile for AWS Batch ECS instance(s)"
+  instance_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    AmazonSSMPatchAssociation    = "arn:aws:iam::aws:policy/AmazonSSMPatchAssociation"
+    nextflowEcsS3AccessPolicy    = aws_iam_policy.nf_ecs_s3_access_policy.arn
+  }
+  instance_iam_role_tags = {
+    Module = "terraform-aws-modules/batch/aws"
+  }
+  instance_iam_role_use_name_prefix = false
 
-    instance_type = [
-      "default_arm64",
-    ]
+  # The module attaches AWSBatchServiceRole automatically
+  service_iam_role_name        = "${var.service_name}BatchServiceRole"
+  service_iam_role_path        = "/batch/"
+  service_iam_role_description = "IAM service role for AWS Batch"
+  service_iam_role_tags = {
+    Module = "terraform-aws-modules/batch/aws"
+  }
+  service_iam_role_use_name_prefix = false
 
-    max_vcpus = 4
-    min_vcpus = 0
+  compute_environments = {
+    nf_ec2_ce = {
+      # Unlike the raw aws_batch_compute_environment resource,
+      # the module appends a '-' to the prefix automatically.
+      name_prefix = "${var.service_name}"
+      compute_resources = {
+        allocation_strategy = "BEST_FIT_PROGRESSIVE"
+        instance_types = [
+          "default_arm64",
+        ]
+        max_vcpus = 4
+        min_vcpus = 0
+        security_group_ids = [
+          data.aws_security_group.nf_sg.id,
+        ]
+        subnets = data.aws_subnets.nf_subnets.ids
+        type    = "EC2"
 
-    security_group_ids = [
-      data.aws_security_group.nf_sg.id,
-    ]
-
-    subnets = data.aws_subnets.nf_subnets.ids
-
-    type = "EC2"
+        # Sets tags on the EC2 instances launched for this compute environment.
+        # Tag changes force compute environment replacement
+        # which can lead to job queue conflicts.
+        # Only specify tags that will be static
+        # for the lifetime of the compute environment.
+        tags = {
+          Name   = var.service_name
+          Module = "terraform-aws-modules/batch/aws"
+          Type   = "Ec2"
+        }
+      }
+    }
   }
 
-  service_role = aws_iam_role.nf_batch_service_role.arn
+  job_queues = {
+    nf_ec2_job_queue = {
+      name     = var.service_name
+      priority = 1
+      state    = "ENABLED"
 
-  type = "MANAGED"
+      compute_environment_order = {
+        0 = {
+          compute_environment_key = "nf_ec2_ce"
+        }
+      }
 
-  depends_on = [
-    # Required to ensure that Batch can manage other AWS services on our behalf.
-    # Annoyingly, if we do not make an explicit dependency,
-    # Batch may silently fail to complete configuration of the downstream ECS cluster
-    # because Terraform has not yet attached the AWSBatchServiceRole policy
-    # to the Batch service role.
-    aws_iam_role_policy_attachment.nf_batch_service_role_batch_service_policy
-  ]
-
-  lifecycle {
-    # Required to decouple compute environment updates
-    # (e.g., changing instance types)
-    # from the job queue
-    create_before_destroy = true
+      create_scheduling_policy = false
+    }
   }
-}
-
-resource "aws_batch_job_queue" "nf_ec2_job_queue" {
-  name  = var.service_name
-  state = "ENABLED"
-
-  compute_environment_order {
-    order               = 1
-    compute_environment = aws_batch_compute_environment.nf_ec2_ce.arn
-  }
-
-  priority = 1
 }
 
 #
 # IAM configuration
 #
 
-# Batch service role
-
-# Allow the Batch service to assume the Batch service role
-data "aws_iam_policy_document" "nf_batch_role_policy_document" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["batch.amazonaws.com"]
-    }
-  }
-}
-
-# Create the Batch service role
-resource "aws_iam_role" "nf_batch_service_role" {
-  name               = "${var.service_name}-BatchServiceRole"
-  assume_role_policy = data.aws_iam_policy_document.nf_batch_role_policy_document.json
-}
-
-# Attach the AWS managed Batch service role policy to the Batch service role.
-# Enables Batch to manage other AWS services on our behalf,
-# (e.g., the EC2 for auto-scaling and instance launching, ECS cluster creating, etc.)
-resource "aws_iam_role_policy_attachment" "nf_batch_service_role_batch_service_policy" {
-  role       = aws_iam_role.nf_batch_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
-}
-
-# ECS instance role
-
-# Allow the EC2 service to assume the ECS instance role
-data "aws_iam_policy_document" "nf_ecs_role_policy_document_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_policy" "nf_ecs_role_policy_document_s3_access" {
-  name = "${var.service_name}-EcsS3AccessPolicy"
+resource "aws_iam_policy" "nf_ecs_s3_access_policy" {
+  name = "${var.service_name}EcsS3Access"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -158,31 +137,6 @@ resource "aws_iam_policy" "nf_ecs_role_policy_document_s3_access" {
       }
     ]
   })
-}
-
-# Create the ECS instance role
-resource "aws_iam_role" "nf_ecs_instance_role" {
-  name               = "${var.service_name}-EcsInstanceRole"
-  assume_role_policy = data.aws_iam_policy_document.nf_ecs_role_policy_document_assume_role.json
-}
-
-# Attach the AWS managed ECS for EC2 role policy to the ECS instance role.
-# Enables EC2 instances to connect to our cluster
-# and register themselves with the ECS control plane.
-resource "aws_iam_role_policy_attachment" "nf_ecs_instance_role_ecs_for_ec2_policy" {
-  role       = aws_iam_role.nf_ecs_instance_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
-
-resource "aws_iam_role_policy_attachment" "nf_ecs_instance_role_s3_access_policy" {
-  role       = aws_iam_role.nf_ecs_instance_role.name
-  policy_arn = aws_iam_policy.nf_ecs_role_policy_document_s3_access.arn
-}
-
-# Create the ECS instance profile to allow EC2 instances to assume the ECS instance role
-resource "aws_iam_instance_profile" "nf_ecs_instance_profile" {
-  name_prefix = "${var.service_name}-"
-  role        = aws_iam_role.nf_ecs_instance_role.name
 }
 
 #
@@ -218,6 +172,7 @@ data "aws_subnets" "nf_subnets" {
 
 #
 # S3 configuration
+# https://github.com/terraform-aws-modules/terraform-aws-s3-bucket
 #
 
 module "nf_s3_bucket" {
